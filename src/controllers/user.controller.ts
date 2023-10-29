39e6615fa6b68c2f102b2bd578f1c97e67b0da83
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-10-29 21:16:28 +0300

    WIP mailer

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from 'express'
 import { TCreateUserSchema } from '../schema/user.schema'
+import { createUser } from '../services/user.service'
 
 export async function createUserHandler(
   req: Request<{}, {}, TCreateUserSchema>,
   res: Response,
 ) {
   const body = req.body
+
+  try {
+    const user = await createUser(body)
+
+    return res.send('Пользователь успешно создан')
+  } catch (error: any) {
+    if (error.code === 11000) {
+      return res.status(409).send('Учетная запись уже существует')
+    }
+
+    return res.status(500).send(error)
+  }
 }
